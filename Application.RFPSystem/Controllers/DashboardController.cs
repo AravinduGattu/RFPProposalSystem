8eb4d325c6d24af61eb8fe58f1fa156592b7163d
Satya Kalasapati <yuvasena.123@gmail.com>
2019-11-27 16:16:51 +0530

    c

--- Application.RFPSystem/Controllers/DashboardController.cs
+++ Application.RFPSystem/Controllers/DashboardController.cs
@@ -38,44 +38,63 @@ namespace Application.RFPSystem.Controllers
 
         [Route("api/V1/GetProposalsGrid")]
         [HttpGet]
-        public async Task<IActionResult> GetProposalsGrid(string requestID)
+        public async Task<IActionResult> GetProposalsGrid(string rfpUser, int? status, int? role)
         {
             try
             {
                 using (var dbComponent = new LiteDatabase(Constants.DBPath))
                 {
-                    List<RFPRequestDataModel> rFPRequestDataModels = new List<RFPRequestDataModel>();
-                    LiteCollection<RFPRequestDataModel> getRequestModels =
-                        dbComponent.GetCollection<RFPRequestDataModel>("RequestProposals");
+                    List<RFPRequestDataGridModel> rFPRequestDataModels = new List<RFPRequestDataGridModel>();
+                    LiteCollection<RFPRequestDataGridModel> getRequestModels =
+                        dbComponent.GetCollection<RFPRequestDataGridModel>("RequestProposals");
 
-                    if (string.IsNullOrEmpty(requestID))
+                    //role and status/user filters cannot be used together
+                    if (role.HasValue && role.Value > 0)
                     {
-                        var listAll = getRequestModels.FindAll().ToList();
-
-
-
-                        listAll.ForEach(x => rFPRequestDataModels.Add(x));
-
-                        return Ok(listAll);
-
+                        ProposalUsers propRole = (ProposalUsers)role;
+                        int[] arr = new int[] { };
+                        switch (propRole)
+                        {
+                            case ProposalUsers.SalesLead:
+                                arr = new int[] { (int)ProposalRequestType.Assigned_To_Delivery_Team,
+                                    (int)ProposalRequestType.Drafted,
+                                    (int)ProposalRequestType.Assigned_To_Delivery_Team };
+                                break;
+                            case ProposalUsers.PracticeLead:
+                                arr = new int[] { (int)ProposalRequestType.Assigned_To_Delivery_Team,
+                                    (int)ProposalRequestType.Drafted,
+                                    (int)ProposalRequestType.Assigned_To_Delivery_Team };
+                                break;
+                            case ProposalUsers.PursuitTeamLead:
+                                arr = new int[] { (int)ProposalRequestType.Assigned_To_Delivery_Team,
+                                    (int)ProposalRequestType.Drafted,
+                                    (int)ProposalRequestType.Assigned_To_Delivery_Team };
+                                break;
+                            case ProposalUsers.DeliveryTeamLead:
+                                arr = new int[] { (int)ProposalRequestType.Assigned_To_Delivery_Team,
+                                    (int)ProposalRequestType.Drafted,
+                                    (int)ProposalRequestType.Assigned_To_Delivery_Team };
+                                break;
+                            default:
+                                break;
+                        }
+                        rFPRequestDataModels = getRequestModels.Find(x =>
+                                arr.Any(p => p.ToString() == x.status)).ToList();
                     }
                     else
                     {
-                        var matchResponse = getRequestModels.Find(x => x.RFPCode.Equals(requestID)).Any();
-
-                        if (matchResponse)
-                        {
-                            var results = getRequestModels.Find(x => x.RFPCode.Equals(requestID)).ToList();
-
-                            results.ForEach(x => rFPRequestDataModels.Add(x));
-
-                            return Ok(results);
-                        }
-                        else
-                        {
-                            return Ok(new { Reason = "Not Found", Response = "No Record on " + requestID });
-                        }
-
+                        rFPRequestDataModels = getRequestModels.Find(x =>
+                        (string.IsNullOrEmpty(rfpUser) || x.RFPCode.Equals(rfpUser, StringComparison.InvariantCultureIgnoreCase))
+                        && (status.HasValue || status.Value == 0 || x.status == status.ToString())).ToList();
+                    }
+                    if (rFPRequestDataModels != null && rFPRequestDataModels.Count > 0)
+                    {
+                        return Ok(rFPRequestDataModels);
+                    }
+                    else
+                    {
+                        return Ok(getRequestModels);
+                        //return Ok(new { Reason = "Not Found", Response = "No Record on " + rfpUser });
                     }
                 }
             }
--- Common.DataObjects/ProposalRequestType.cs
+++ Common.DataObjects/ProposalRequestType.cs
@@ -4,10 +4,36 @@ using System.Text;
 
 namespace Common.DataObjects
 {
+    //public enum ProposalRequestType
+    //{
+    //    Project = 1,
+    //    Resource = 2
+    //}
+
     public enum ProposalRequestType
     {
-        Project=1,
-        Resource=2
+        Drafted = 1,
+        //PRLApprove
+        Draft_Reviewed = 2,
+        //PRLReject
+        Draft_Rejected = 3,
+        Draft_Resubmit = 4,
+        //PULApprove
+        Solution_Created = 5,
+        //PRLApprove
+        Solution_Reviwed = 6,
+        //PRLApprove
+        Pricing_Created = 7,
+        //PRLReject
+        Solution_Rejected = 8,
+        //PRLReject
+        Solution_Resubmit = 9,
+        //SL
+        Presentation_Submited = 10,
+        //PRL 
+        Assigned_To_Delivery_Team = 11,
+        //DL
+        Accept = 12
     }
 
     public static class Constants
