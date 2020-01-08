e5014a99e4ef478e19fc0e2cd55a81f16f85b59c
bpatnala <56917517+bpatnala@users.noreply.github.com>
2020-01-08 18:46:12 +0530

    Project Role service

---
+++ Application.RMGSystem/Controllers/RoleController.cs
@@ -0,0 +1,82 @@
+﻿using App.RFPSystem.Services;
+using App.RFPSystem.Services.RMG;
+using Applications.Operations;
+using Common.DataObjects;
+using Common.DataObjects.RFP;
+using Common.DataObjects.RMG;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.RMGSystem.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class RoleController : BaseController
+    {
+        [Route("api/V1/RoleMaster/GetList")]
+        [HttpGet]
+        public async Task<IActionResult> GetList(int? id, string name, string status, string startDate, string endDate)
+        {
+            try
+            {
+                IEnumerable<RoleMaster> list  = new List<RoleMaster>();
+
+                using (ISyncRoleMaster service = new RoleService())
+                {
+                    list = await service.GetList(id ?? 0, name, status, startDate, endDate);
+                }
+
+                return Ok(list);
+            }
+            catch (System.Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+
+        [Route("api/V1/RoleMaster/Save")]
+        [HttpPost]
+        public async Task<IActionResult> Save(RoleMaster item)
+        {
+            try
+            {
+                bool status = false;
+                using (ISyncRoleMaster service = new RoleService())
+                {
+                    item.CreatedBy = item.ModifiedBy = UserID.ToString();
+                    status = await service.Save(item) > 0;
+                }
+
+                return Ok(status);
+            }
+            catch (System.Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+
+        [Route("api/V1/RoleMaster/Delete")]
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                bool status = false;
+                using (ISyncRoleMaster service = new RoleService())
+                {
+                    status = await service.Delete(id) > 0;
+                }
+
+                return Ok(status);
+            }
+            catch (System.Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+    }
+}
---
+++ Common.DataObjects/RMG/RoleMaster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.DataObjects.RMG
+{
+    public class RoleMaster : AuditFields
+    {
+        public int Role_ID { get; set; }
+        public string Role_Name { get; set; }
+        public string Role_Status { get; set; }
+        public DateTime Role_StartDate { get; set; }
+        public DateTime Role_EndDate { get; set; }
+        public int Role_Flag { get; set; }
+    }
+}
