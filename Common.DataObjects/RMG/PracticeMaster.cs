ecc89c195cc271e92c8d5f8d89a78efc6487752c
bpatnala <56917517+bpatnala@users.noreply.github.com>
2020-01-09 12:28:04 +0530

    Edge Practice Service

---
+++ Common.DataObjects/RMG/PracticeMaster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.DataObjects.RMG
+{
+    public class PracticeMaster : AuditFields
+    {
+        public int Practice_ID { get; set; }
+        public string Practice_Description { get; set; }
+        public string Practice_Status { get; set; }
+        public DateTime Practice_StartDate { get; set; }
+        public DateTime Practice_EndDate { get; set; }
+        public int Practice_Flag { get; set; }
+    }
+}
